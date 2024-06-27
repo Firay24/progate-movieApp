80eb830fea0c3ce90000b9c5c6d796288b8f5e05
Hatakee Fira <fisan.onodaconan@gmail.com>
2024-06-27 23:49:28 +0700

    fetching data

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -2,10 +2,34 @@ import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 
 export default function MovieDetail({ navigation }: any): JSX.Element {
+  const fetchData = (): void => {
+    const ACCESS_TOKEN =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTIwNTMwODYyMzQ0OGQyNDMyYzk5NmZkM2Y3M2Y3ZSIsIm5iZiI6MTcxOTUwNjM5MS4yNDM1NzQsInN1YiI6IjY2N2Q5MTM4ZGY5MzE4ZGEzMmFmOWEwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mx96l9oqlTPXIS57gSMT6VhiCj1KPprTbCcPq6hofnc'
+    const url =
+      'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+    }
+
+    fetch(url, options)
+      .then(async (response) => await response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Detail</Text>
-      <Button title="Kembali" onPress={() => navigation.navigate('Home')} />
+      <Text>Movie Detail Page</Text>
+      <Button title="Fetch Data" onPress={() => fetchData()} />
     </View>
   )
 }
